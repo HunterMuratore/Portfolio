bdf11e35a046d240bc7fb6fe04df5aac1d133760
Hunter <muratoreh@gmail.com>
2023-11-10 15:26:35 -0500

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import headshot from '../images/Hunter_Headshot.jpg'
+
 function Home() {
     return (
-        <section className="home text-center mb-3">
-            <h1 className="text-3xl font-bold mt-10 mb-5">Hunter Muratore</h1>
-            <h2 className="text-3xl font-bold mb-5">Full Stack Developer</h2>
+        <section className="home flex flex-col items-center gap-6 text-center mb-3">
+            <div className="mx-auto">
+                <div>
+                    <h1 className="text-3xl font-bold mt-10 mb-5">Hunter Muratore</h1>
+                    <hr className="w-[300px] mx-auto" />
+                    <h2 className="text-3xl font-bold mt-5 mb-10">Full Stack Developer</h2>
+                </div>
+                <img className='max-h-[500px]' src={headshot} alt="Hunter Headshot" />
+            </div>
         </section>
     )
 }
