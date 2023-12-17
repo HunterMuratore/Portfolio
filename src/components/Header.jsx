4a50983853bcc00fbdc2cb98af03243c8012284a
Hunter <muratoreh@gmail.com>
2023-12-16 19:41:39 -0500

    experience dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,56 @@
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
-import logo from '../images/logo-purple.png'
+import logo from '../images/logo-purple.png';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
-    return (
-        <header className="flex items-center justify-between py-1 pl-3 pr-3">
-            <NavLink to="/"><img className="max-h-[36px] lg:max-h-[52px]" src={logo} alt="HMM Logo" /></NavLink>
+  const [isExperienceOpen, setIsExperienceOpen] = useState(false);
 
-            <nav className="text-sm sm:text-md lg:text-lg">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/projects">Projects</NavLink>
-                <NavLink to="/resume">Resume</NavLink>
-                <NavLink to="/certificate">Certificate</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-            </nav>
-        </header>
-    )
+  const toggleExperienceDropdown = () => {
+    setIsExperienceOpen(!isExperienceOpen);
+  };
+
+  const closeExperienceDropdown = () => {
+    setIsExperienceOpen(false);
+  };
+
+  return (
+    <header className="flex items-center justify-between py-1 pl-3 pr-3">
+      <NavLink to="/">
+        <img className="max-h-[36px] lg:max-h-[52px]" src={logo} alt="HMM Logo" />
+      </NavLink>
+
+      <nav className="text-sm sm:text-md lg:text-lg flex items-center">
+        <NavLink to="/" onClick={closeExperienceDropdown}>Home</NavLink>
+        <NavLink to="/about" onClick={closeExperienceDropdown}>About</NavLink>
+        <NavLink to="/projects" onClick={closeExperienceDropdown}>Projects</NavLink>
+
+        <div className='relative'>
+          <div onClick={toggleExperienceDropdown} className="cursor-pointer text-white">
+            Experience <FontAwesomeIcon className="dropdown" icon={isExperienceOpen ? faCaretUp : faCaretDown} />
+          </div>
+
+          {isExperienceOpen && (
+            <div className="flex flex-col absolute mt-2 bg-white border border-gray-200 rounded shadow-lg">
+              <NavLink to="/resume" onClick={closeExperienceDropdown} className="px-3 py-2 text-gray-800">
+                Resume
+              </NavLink>
+              <NavLink to="/certificate" onClick={closeExperienceDropdown} className="px-3 py-2 text-gray-800">
+                Certificate
+              </NavLink>
+            </div>
+          )}
+        </div>
+
+        <NavLink to="/contact" onClick={closeExperienceDropdown}>
+          Contact
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
